1125d4e4cdc74d1a541ae3bf158c55d471bf4608
kik8727 <kik87827@gmail.com>
2024-07-02 00:37:39 +0900

    모달 수정

--- event_01/assets/js/front.js
+++ event_01/assets/js/front.js
@@ -156,3 +156,99 @@ class DesignPopup {
       }
     }
   }
+
+
+  function designModal(option){
+    const modalGroupCreate = document.createElement("div");
+    let modal_wrap_parent = null;
+    let modal_item = null;
+    let pagewrap = document.querySelector(".page_wrap");
+    let showNum = 0;
+    modalGroupCreate.classList.add("modal_wrap_parent");
+
+    if (!modal_wrap_parent && !document.querySelector(".modal_wrap_parent")) {
+      pagewrap.append(modalGroupCreate);
+    }else{
+      modalGroupCreate.remove();
+    }
+    modal_wrap_parent = document.querySelector(".layer_wrap_parent");
+
+    let btnHTML = ``;
+
+    if(option.modaltype === "confirm"){
+      btnHTML = `
+        <a href="#" class="btn_modal_submit primary cancelcall"><span class="btn_modal_submit_text">취소</span></a>
+        <a href="#" class="btn_modal_submit primary okcall"><span class="btn_modal_submit_text">확인</span></a>
+      `;
+    }else{
+      btnHTML = `
+        <a href="#" class="btn_modal_submit primary okcall"><span class="btn_modal_submit_text">확인</span></a>
+      `;
+    }
+
+    let modal_template = `
+      <div class="modal_wrap">
+          <div class="bg_dim"></div>
+          <div class="modal_box_tb">
+              <div class="modal_box_td">
+                  <div class="modal_box_item">
+                      <div class="modal_box_message_row">
+                          <p class="modal_box_message">${option.message}</p>
+                      </div>
+                      <div class="btn_modal_submit_wrap">
+                          ${btnHTML}
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+    `;
+    modal_wrap_parent.innerHTML = modal_template;
+    modal_item = modal_wrap_parent.querySelector(".modal_wrap");
+    modal_item.classList.add("active");
+    if(showNum){clearTimeout(showNum);}
+    showNum = setTimeout(()=>{
+      modal_item.classList.add("motion_end");
+      modal_item.addEventListener("transitionend",(e)=>{
+        if(e.currentTarget.classList.contains("motion_end")){
+          if(option.showCallback){
+            option.showCallback();
+          }
+        }
+      });
+    },10);
+
+    let btn_modal_submit = modal_item.querySelectorAll(".btn_modal_submit");
+    if(!!btn_modal_submit){
+      btn_modal_submit.forEach((item)=>{
+        let eventIs = false;
+        if(eventIs){
+          item.removeEventListener("click");
+        }
+        item.addEventListener("click",(e)=>{
+          let thisTarget = e.currentTarget;
+          closeAction();
+          if(thisTarget.classList.contains("okcall")){
+            if(option.okcallback){
+              option.okcallback();
+            }
+          }else if(thisTarget.classList.contains("cancelcall")){
+            if(option.cancelcallback){
+              option.cancelcallback();
+            }
+          }
+          eventIs = true;
+        });
+      });
+    }
+
+    function closeAction(){
+      let actionNum = 0;
+      modal_item.classList.remove("motion_end");
+      if(actionNum){clearTimeout(actionNum);}
+      actionNum = setTimeout(()=>{
+        modal_item.classList.remove("active");
+        modal_item.remove();
+      },500);
+    }
+  }
